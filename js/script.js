908d3d4283704518af1a2b67b168ad5dc8c6dcb7
verpenunes <106631829+verpenunes@users.noreply.github.com>
2024-02-26 21:49:09 +0100

    close nav

--- js/script.js
+++ js/script.js
@@ -4,53 +4,63 @@ const modalImg = document.getElementById("modalImage");
 const closeButton = document.querySelector(".closeModal");
 
 function openModal(imageSrc) {
-    modal.style.display = "block";
-    modalImg.src = imageSrc;
-    console.log(openModal);
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+  console.log(openModal);
 }
 
 function closeModal() {
-    modal.style.display = "none";
-    console.log(closeModal);
+  modal.style.display = "none";
+  console.log(closeModal);
 }
 
 document.querySelector(".modal-container").addEventListener("click", (e) => {
-    e.preventDefault();
-    if (e.target.tagName === "IMG") {
-        openModal(e.target.src);
-    }
+  e.preventDefault();
+  if (e.target.tagName === "IMG") {
+    openModal(e.target.src);
+  }
 });
 
 document.querySelector(".modal-container-2").addEventListener("click", (e) => {
-    e.preventDefault();
-    if (e.target.tagName === "IMG") {
-        openModal(e.target.src);
-    }
+  e.preventDefault();
+  if (e.target.tagName === "IMG") {
+    openModal(e.target.src);
+  }
 });
 
 document.querySelector(".modal-container-3").addEventListener("click", (e) => {
-    e.preventDefault();
-    if (e.target.tagName === "IMG") {
-        openModal(e.target.src);
-    }
+  e.preventDefault();
+  if (e.target.tagName === "IMG") {
+    openModal(e.target.src);
+  }
 });
 
 closeButton.addEventListener("click", closeModal);
 
 window.addEventListener("click", (e) => {
-    if (e.target === modal) {
-        closeModal();
-    }
+  if (e.target === modal) {
+    closeModal();
+  }
 });
 
 /* Nav Menu */
 function toggleMenu() {
-    const menuMobile = document.getElementById("menu-mobile")
+  const menuMobile = document.getElementById("menu-mobile");
 
-    if(menuMobile.className === "menu-mobile-active") {
-        menuMobile.className = "menu-mobile"
-    } else {
-        menuMobile.className = "menu-mobile-active"
-    }
+  if (menuMobile.className === "menu-mobile-active") {
+    menuMobile.className = "menu-mobile";
+  } else {
+    menuMobile.className = "menu-mobile-active";
+  }
 }
 
+const close = document.querySelector(".text-close");
+
+close.addEventListener("mouseover", function () {
+  close.style.color = "lightgrey";
+  //console.log(underline);
+});
+
+close.addEventListener("mouseout", function () {
+  close.style.color = "";
+});
